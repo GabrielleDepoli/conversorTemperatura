189fff439fe4cf90bd291b858c2b28987895fd56
GabrielleDepoli <gabriellefernandesdepoli@gmail.com>
2025-04-24 11:36:49 -0300

    Commit inicial da Gabi.

---
+++ Conversor.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Conversor{
+    public static void main(String[] args) {
+        //Declaração de variáveis
+        double cel, fah; //cel - temperatura em graus celsius; fah - temperatura em graus fahrenheit
+
+        //Criação e instância do objeto de entrada
+        Scanner entrada = new Scanner(System.in);
+
+        //Apresentação do programa
+        System.out.println("\n\t\t\t -- Conversor --\n");
+
+        //Entradas
+        System.out.print("Informe a temperatura em ºC: ");
+        cel = entrada.nextDouble();
+
+        //Processamento
+        fah = cel * 9 / 5 + 32;
+
+        //Saída
+        System.out.println("\n" + cel + "ºC = " + fah + "ºF\n");
+    }
+}
