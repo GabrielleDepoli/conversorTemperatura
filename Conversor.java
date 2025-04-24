c9d28de6aa6cf0519edeb41923e10a8aaa2532de
seffany-oliv <steffany.oliv1@gmail.com>
2025-04-24 15:12:56 -0300

    commit steffany: Menu

--- Conversor.java
+++ Conversor.java
@@ -4,6 +4,7 @@ public class Conversor{
     public static void main(String[] args) {
         //Declaração de variáveis
         double cel, fah; //cel - temperatura em graus celsius; fah - temperatura em graus fahrenheit
+        int op; 
 
         //Criação e instância do objeto de entrada
         Scanner entrada = new Scanner(System.in);
@@ -11,14 +12,32 @@ public class Conversor{
         //Apresentação do programa
         System.out.println("\n\t\t\t -- Conversor --\n");
 
-        //Entradas
-        System.out.print("Informe a temperatura em ºC: ");
-        cel = entrada.nextDouble();
+        //Menu
+        System.out.println("1. ºC para ºF");
+        System.out.println("2. ºF para ºC");
+        System.out.println("3. Sair");
 
-        //Processamento
-        fah = cel * 9 / 5 + 32;
+        //Opição de usuário
+        System.out.println("/nOpsão");
+        op = entrada.nextInt();
 
-        //Saída
-        System.out.println("\n" + cel + "ºC = " + fah + "ºF\n");
+        if op == 1;{
+            //Entradas
+            System.out.print("Informe a temperatura em ºC: ");
+            cel = entrada.nextDouble();
+
+            //Processamento
+            fah = cel * 9 / 5 + 32;
+
+            //Saída
+            System.out.println("\n" + cel + "ºC = " + fah + "ºF\n");
+
+        }else if (op = 2){
+            //Convensão para fahrenheit
+        }else if (op = 3){
+            System.out.println("Forte abraço!!!");
+        }else{
+            System.out.println("Opção" + op + "incorreta");
+        }
     }
 }
