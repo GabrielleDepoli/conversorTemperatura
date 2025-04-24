cc7dbf954227a01b08723d81841696570e4bd2fe
Isabela-Rods <isabela.rodrigues172008@gmail.com>
2025-04-24 16:37:07 -0300

    Commit da isa: Inclusão da 2 parte

--- Conversor.java
+++ Conversor.java
@@ -4,7 +4,7 @@ public class Conversor{
     public static void main(String[] args) {
         //Declaração de variáveis
         double cel, fah; //cel - temperatura em graus celsius; fah - temperatura em graus fahrenheit
-        int op; 
+        int op; //Opção do usuário 
 
         //Criação e instância do objeto de entrada
         Scanner entrada = new Scanner(System.in);
@@ -17,14 +17,16 @@ public class Conversor{
         System.out.println("2. ºF para ºC");
         System.out.println("3. Sair");
 
-        //Opição de usuário
-        System.out.println("/nOpsão");
+        //Opção de usuário
+        System.out.println("\nOpção");
         op = entrada.nextInt();
 
-        if op == 1;{
+        if (op == 1) {
+            System.out.println("Conversão para Fahrenheit");
+
             //Entradas
-            System.out.print("Informe a temperatura em ºC: ");
-            cel = entrada.nextDouble();
+            System.out.println("Informe a temperatura em ºC: ");
+            cel = entrada.nextInt();
 
             //Processamento
             fah = cel * 9 / 5 + 32;
@@ -32,12 +34,27 @@ public class Conversor{
             //Saída
             System.out.println("\n" + cel + "ºC = " + fah + "ºF\n");
 
-        }else if (op = 2){
-            //Convensão para fahrenheit
-        }else if (op = 3){
+        } else if (op == 2) {
+            //Converter Farenheit para Celsius
+            System.out.println("Conversão para Celsius\n");
+
+            //Entradas
+            System.out.println("Informe a temperatura em ºF: ");
+            fah = entrada.nextInt();
+
+            //Processamento
+            cel = (fah - 32) * 5 / 9;
+
+            //Saída
+            System.out.println("\n" + fah + "ºF = " + cel + "ºC\n");
+
+
+        } else if (op == 3) {
             System.out.println("Forte abraço!!!");
-        }else{
+
+        } else {
             System.out.println("Opção" + op + "incorreta");
-        }
+        
+        }while (op!=3);
     }
 }
